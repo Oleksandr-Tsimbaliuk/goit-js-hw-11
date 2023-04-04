6174b59aeb2aa78eef66a37cf3f8b8fdcb109f8e
Oleksandr <115025828+g0d2illa@users.noreply.github.com>
2023-04-04 22:15:07 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import PixabayAPI from './pixabay-api';
+import cardMarkup from './cardMarkup';
 
-const DEBOUNCE_DELAY = 300;
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const pixabayAPI = new PixabayAPI();
+
+formEl.addEventListener('submit', onFormElSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+function onFormElSubmit(event) {
+  event.preventDefault();
+  pixabayAPI.query = event.target.elements.searchQuery.value;
+  pixabayAPI.page = 1;
+  loadMoreBtn.classList.remove('hidden');
+
+  if (pixabayAPI.query === '') {
+    Notify.warning('Вы ввели пустой запрос, проверьте правильность ввода');
+    return;
+  }
+
+  pixabayAPI.fetchPhotos().then(({ data }) => {
+    pixabayAPI.hitsCounter = pixabayAPI.hits;
+
+    console.log(data);
+
+    console.log(`totalHits: ${data.totalHits}`);
+    console.log(`hits: ${pixabayAPI.hits}`);
+    console.log(`counter: ${pixabayAPI.hitsCounter}`);
+    console.log(data.hits);
+
+    galleryEl.innerHTML = cardMarkup(data.hits);
+  });
+}
+
+function onLoadMoreBtn() {
+  pixabayAPI.page += 1;
+  pixabayAPI.hitsCounter += pixabayAPI.hits;
+
+  pixabayAPI.fetchPhotos().then(({ data }) => {
+    if (data.totalHits / pixabayAPI.hitsCounter <= 1) {
+      Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    console.log(`totalHits: ${data.totalHits}`);
+    console.log(`hits: ${pixabayAPI.hits}`);
+    console.log(`counter: ${pixabayAPI.hitsCounter}`);
+    console.log(data.hits);
+
+    galleryEl.insertAdjacentHTML('beforeend', cardMarkup(data.hits));
+  });
+}
